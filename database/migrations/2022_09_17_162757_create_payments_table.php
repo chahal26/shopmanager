cb95d539fd99c42d9e6f36202ad29adc500f93f9
Sahil Chahal <dev.sahilchahal@gmail.com>
2022-09-17 09:32:42 -0700

    Added Payment Model & MIgration

---
+++ database/migrations/2022_09_17_162757_create_payments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('customer_id')->unsigned();
+            $table->integer('amount_paid')->default(0);
+            $table->string('payment_mode')->default('cash');
+            $table->timestamps();
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
