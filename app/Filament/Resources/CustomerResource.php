d9bd2ee3b22ddea183e5a84f588268c4904eac11
Sahil Chahal <dev.sahilchahal@gmail.com>
2022-10-01 10:55:27 -0700

    Added Customer Icon

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -20,7 +20,7 @@ class CustomerResource extends Resource
 {
     protected static ?string $model = Customer::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-collection';
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
 
     public static function form(Form $form): Form
     {
